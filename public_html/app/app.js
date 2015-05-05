f05759dcbeaa74cc6cdbf70baf44df2409720da1
lomagno2003 <lomagno2003@gmail.com>
2015-05-05 19:47:17 -0300

    Home casi terminado

--- public_html/app/app.js
+++ public_html/app/app.js
@@ -49,7 +49,14 @@ app.run(function($rootScope) {
 				"epigraph":"Picture 3",
 			     "url" : "http://wallpapers111.com/wp-content/uploads/2015/03/Cars-Desktop-Wallpaper-HD3.jpg",
 			}],
-			"description" : "This is a long description of the news, it's so long that couldn't been displayed in the news thumbnail, so it should be cutted off"
+			"description" : 
+ "TLorem ipsum dolor sit celerisque metus. Ut scelerisque, nibh non lacinia justo gravida eros, nec auctor tortor libero at elit. Proin imperdiet augue at diam convallis vestibulum. Donec tristique id urna ut semper. \n"
+ + "\n"
+ + "Curabitur mattis nisi arcu, eget suscipit turpis posuere quis. Morbi blandit gravida velit, nec efficitur nisl mattis vitae. Aliquam pretium, purus eget iaculis consequat, dui sem egestas risus, tempor consectetur ligula eros a nisi. Integer gravida rutrum mi, quis pellentesque magna auctor quis. Aenean a gravida massa, ac tristique risus. Maecenas molestie eget turpis vel volutpat. Nullam commodo arcu id dictum tempus. Sed pulvinar nisi metus, eu dapibus sapien hendrerit nec. Phasellus blandit sodales ante, eu commodo dui lacinia auctor.\n"
+ + "\n"
+ + "Vivamus in auctor dui, vitae volutpat lectus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur non felis sed nulla elementum sagittis pellentesque et neque. Phasellus condimentum semper vestibulum. Fusce a vehicula felis. Aliquam non volutpat dui. Etiam sollicitudin dui nec turpis placerat, sit amet porta neque mattis. Nunc blandit id erat sit amet eleifend. Maecenas mi enim, commodo id diam at, porttitor scelerisque libero. Donec sed ligula auctor, suscipit turpis eget, blandit velit. Donec iaculis odio a lacus lobortis, ut accumsan neque vestibulum. Fusce a enim pulvinar, semper metus mollis, facilisis urna. Nullam id urna rhoncus, pellentesque libero ac, fermentum orci. Suspendisse nec sapien eget ex mollis pulvinar.\n"
+ + "\n"
+ + "Fusce in odio suscipit, rutrum quam ut, hendrerit tortor. Aenean ullamcorper odio quam, ut dictum est vulputate id. Vestibulum laoreet ligula quis sapien interdum, ut tincidunt lectus pharetra. Mauris laoreet, massa ut iaculis efficitur, tellus ex pellentesque dui, non scelerisque lectus nulla non augue. Vestibulum nec ipsum suscipit, finibus ligula a, tincidunt dolor. Nulla eros lacus, eleifend et gravida eget, ornare eu velit. Nulla tristique eros ac lacus varius iaculis. Nam libero ligula, viverra tempus dui sit amet, auctor imperdiet ligula. Vivamus sed nibh vel urna luctus accumsan. Fusce porta tempor magna, quis congue dolor sagittis sed.\n"
 		},
 		{
 			"id":1,
--- public_html/app/common/directives/newsDirective/newsDirective.js
+++ public_html/app/common/directives/newsDirective/newsDirective.js
@@ -10,16 +10,6 @@ app.directive('newsThumbnail', function($rootScope) {
 			//TODO: The news information should be retrieved here from the server
 			scope.news = $rootScope.dummyNews[scope.newsId];
 			
-			//TODO: This value should be stored in a global scope
-			var maxDescriptionLength = 50;
-			
-			/* Cut off the description if nescesary */
-			if(scope.news.description.length > maxDescriptionLength){
-				scope.news.shortDescription = scope.news.description.substring(0, maxDescriptionLength - 3) + '...';
-			} else {
-				scope.news.shortDescription = scope.news.description;
-			}
-			
 			/* Set the thumbnail image */
 			if(scope.news.media && scope.news.media.lenght != 0){
 				scope.news.thumbnailUrl = scope.news.media[0].url;
