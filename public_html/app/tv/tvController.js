477754dc2bf10dba5c0d88f52241edc079d75d67
lomagno2003 <lomagno2003@gmail.com>
2015-06-15 17:17:11 -0300

    Auto-load videos from YouTube

--- public_html/app/tv/tvController.js
+++ public_html/app/tv/tvController.js
@@ -1,5 +1,21 @@
 var app = angular.module('app');
 
-app.controller('tvController', function($scope) {
+app.controller('tvController', function($scope, $http) {
+	$scope.videos = [];
+	$scope.key = "AIzaSyAzrhQr6XJq0HFcxL3q5Nb1cA85zEb8Khw";
+	$scope.channelId = "UCOn9leGj1xnJ5vkKUDDasSw";
+
+	$scope.url = "https://www.googleapis.com/youtube/v3/search?key=" + 
+		$scope.key + 
+		"&channelId=" + 
+		$scope.channelId + 
+		"&part=snippet,id&order=date&maxResults=20";
 	
+	$http.get($scope.url).
+	  success(function(data, status, headers, config) {
+	    $scope.videos = data["items"];
+	  }).
+	  error(function(data, status, headers, config) {
+	    console.error(status);
+	  });
 });
