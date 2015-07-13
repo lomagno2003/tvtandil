bdc36046c4fac90812ce8c7b75ecec446dcc4e3f
lomagno2003 <lomagno2003@gmail.com>
2015-07-13 19:13:45 -0300

    Fix en newsDetail

--- public_html/app/newsDetail/newsDetailController.js
+++ public_html/app/newsDetail/newsDetailController.js
@@ -2,7 +2,7 @@ var app = angular.module('app');
 
 app.controller('newsDetailController', function($scope, $rootScope, $routeParams, Restangular) {
 	Restangular.one("news", $routeParams.newsId).get().then(function(data){
-		$scope.actualNews = data	
+		$scope.actualNews = data.data;
 	});
 	
 	$scope.sync = function(){
